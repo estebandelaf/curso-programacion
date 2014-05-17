47907b29aa97162290fb26c3fddcf855fc78f344
Roberto Moreno <rampmaster@gmail.com>
2014-05-17 15:12:22 -0400

    Datos de prueba para las clases del inventario

---
+++ laboratorios/2014-1/vespertino/lab05/src/datosPrueba.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+listado_producto = [
+        {
+            "nombre"      :"Abaco de colores",
+            "sku"         :"ABA25341",
+            "disponibles" :15,
+        },
+        {
+            "nombre"      :"Abecedario plegable",
+            "sku"         :"ABA255353253",
+            "disponibles" :2,
+        },
+        {
+            "nombre"      :"Cuaderno College",
+            "sku"         :"CUA25341",
+            "disponibles" :100,
+        },
+        {
+            "nombre"      :"Libro de Lenguaje para primero basico",
+            "sku"         :"LIB25341",
+            "disponibles" :15,
+        },
+        {
+            "nombre"      :"Libro de Matematicas para primero basico",
+            "sku"         :"LIB223341",
+            "disponibles" :19,
+        }
+    ]
+
+listado_servicios = [
+        {
+            "nombre"      :"Reparto a Domicilio",
+            "codigo"      :1,
+            "disponibles" :1,
+        },
+        {
+            "nombre"      :"Despacho programado",
+            "sku"         :2,
+            "disponibles" :1,
+        },
+        {
+            "nombre"      :"Entrega en el dia",
+            "sku"         :3,
+            "disponibles" :1,
+        },
+        {
+            "nombre"      :"Entrega en horario no habil",
+            "sku"         :4,
+            "disponibles" :1,
+        },
+        {
+            "nombre"      :"Entrega en disfraz de conejo",
+            "sku"         :5,
+            "disponibles" :1,
+        }
+    ]
