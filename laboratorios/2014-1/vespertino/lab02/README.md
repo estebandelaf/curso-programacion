5b2e9025b7c17616082c8c1d72135a0821c2971b
Roberto Moreno <rampmaster@gmail.com>
2014-05-24 09:43:41 -0400

    Se sube ejemplos pendientes de herencia y comaparativa

--- laboratorios/2014-1/vespertino/lab02/README.md
+++ laboratorios/2014-1/vespertino/lab02/README.md
@@ -20,5 +20,5 @@ Acuerdo para las próximas clases
 - 10-05-2014: Programación Orientada a Objetos 1 a las 10:20 hrs. (Recuperativa)
 - 10-05-2014: Programación Orientada a Objetos 2 a las 14:00 hrs. (Clase Normal, se puede adelantar al modulo anterior previo acuerdo en el bloque de las 10:20 hrs.)
 - 10-05-2014: Entrega de Enunciado de Tarea 3 - POO(Durante la Tarde, si no recordar por email)
-- 14-05-2014: Entrega de Enunciado de Tarea 4 - Lenguaje C (Durante la Tarde, si no recordar por email)
+- 17-05-2014: Entrega de Enunciado de Tarea 4 - Lenguaje C (Durante la Tarde, si no recordar por email)
 - 24-05-2014: Entrega de Tarea 3
---
+++ laboratorios/2014-1/vespertino/lab03-04/src/01_procedural.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Ejemplo de inicialización
+alumno = {
+    "id":1,
+    "nombre":"Alumno",
+    "email":"alumnos@uandresbello.edu",
+    "nota":7,
+}
+profesor = {
+    "id":1,
+    "nombre":"Profesor",
+    "email":"profesor@uandresbello.edu",
+    "evaluacionAcademica":7,
+}
+clase = {
+    "nombre":"",
+    "codigo":"",
+    "alumnos":[],
+    "profesores":[]
+}
+
+# Ejemplo de arreglos
+
+clase = {
+    "nombre":"",
+    "codigo":"",
+    "alumnos":[
+        {
+            "id":1,
+            "nombre":"Alumno",
+            "email":"alumnos@uandresbello.edu",
+            "nota":7,
+        },
+        {
+            "id":2,
+            "nombre":"Alumna",
+            "email":"alumna@uandresbello.edu",
+            "nota":7,
+        }
+    ],
+    "profesores":[
+        {
+            "id":1,
+            "nombre":"Profesor",
+            "email":"profesor@uandresbello.edu",
+            "evaluacionAcademica":7,
+        }
+    ]
+}
