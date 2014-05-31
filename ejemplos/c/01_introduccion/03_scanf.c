9bef336aaaaadeb65ef2aa2d96be0d839aa321f6
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-05-31 16:33:52 -0400

    agregados ejemplos de entrada al programa y macros

---
+++ ejemplos/c/01_introduccion/03_scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n); /* WARNING: es recomendable usar fgets!!! */
+	printf("n = %d\n", n);
+	return EXIT_SUCCESS;
+}
---
+++ ejemplos/c/01_introduccion/04_macros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14
+#define cuadrado(x) x*x
+#define suma(x,y) x+y
+#define suma2(x,y) (x+y)
+
+int main()
+{
+	printf("PI = %.2f\n", PI);
+	printf("cuadrado(2) = %d\n", cuadrado(2));
+	/* las macros son reemplazadas por preprocesador antes de compilar
+	 *    printf("PI = %.2f\n", 3.14);
+	 *    printf("cuadrado(2) = %d", 2*2);
+	 */
+	printf("suma(1,2) = %d\n", suma(1,2));
+	printf("2*suma(1,2) = %d\n", 2*suma(1,2));
+	printf("2*suma2(1,2) = %d\n", 2*suma2(1,2));
+	return EXIT_SUCCESS;
+}
